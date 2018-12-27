0272ef41001f0a34569a8183b26ade8364cc9fbe
d.frongillo <d.frongillo@list-group.com>
2018-12-27 19:35:05 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wijmodemo';
+  value;
+  cd() {
+    console.log('run change detection')
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WjInputModule } from 'wijmo/wijmo.angular2.input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WjInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
